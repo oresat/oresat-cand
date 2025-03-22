c7ead1c9568f201cbcd6fd3f865de7e1cd1d6e0e
Ryan Medick <rmedick@protonmail.com>
2025-03-22 14:44:54 -0700

    remove internal od from py lib

--- libcanopend-py/oresat_libcanopend/__init__.py
+++ libcanopend-py/oresat_libcanopend/__init__.py
@@ -1,12 +1,4 @@
 from .entry import DataType, Entry, EntryBitField
-from .gen.od import (
-    Mission,
-    SoftwareEntry,
-    SoftwareTpdo,
-    SystemBootSelectBitField,
-    SystemReset,
-    UpdaterStatus,
-)
 from .node_client import NodeClient, NodeState
 
 try:
@@ -18,13 +10,7 @@ __all__ = [
     "DataType",
     "Entry",
     "EntryBitField",
-    "Mission",
     "NodeClient",
     "NodeState",
-    "SoftwareEntry",
-    "SoftwareTpdo",
-    "SystemBootSelectBitField",
-    "SystemReset",
-    "UpdaterStatus",
     "__version__",
 ]
