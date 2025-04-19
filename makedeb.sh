50a05de0d4afc9a82c9d63ca7dbf21a3acf2de6b
Ryan Medick <rmedick@protonmail.com>
2025-04-19 14:08:37 -0700

    meson gets project version from git

--- makedeb.sh
+++ makedeb.sh
@@ -6,7 +6,7 @@ VERSION=`git describe --tags --abbrev=0`
 VERSION="${VERSION:1}"  # remove leading 'v'
 
 cat > "debian/changelog" <<-__EOF__
-$PACKAGE ($VERSION-0) buster; urgency=low
+$PACKAGE ($VERSION-0) bullseye; urgency=low
 
   * See git logs at https://github.com/oresat/$PACKAGE/releases
 
